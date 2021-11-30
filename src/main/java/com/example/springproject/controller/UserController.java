2c06aa9582685e08c5d4e19ad082f61339bef31d
Simon Anveden <73692385+anveden@users.noreply.github.com>
2021-11-30 11:22:14 +0100

    fixed some stuff that went wrong last push

--- src/main/java/com/example/springproject/controller/UserController.java
+++ src/main/java/com/example/springproject/controller/UserController.java
@@ -1,16 +1,13 @@
 package com.example.springproject.controller;
 
+import com.example.springproject.data.mapper.UserMapper;
 import com.example.springproject.entity.UserDto;
 import com.example.springproject.response.UserResponse;
 import com.example.springproject.service.UserService;
-import jakarta.ws.rs.Produces;
-import org.apache.catalina.User;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -24,27 +21,32 @@ public class UserController {
     }
 
     //Returns all users in XML format
-    @GetMapping(value = "/users-xml", produces = { "application/xml" })
+    @GetMapping(value = "/users/getAll/xml", produces = { "application/xml" })
     public List<UserDto> getAllUsersXML() {
         return userService.getAll();
     }
 
-    @GetMapping("/users")
+    @GetMapping(value="/users/getAll", produces = { "application/json" })
     public List<UserDto> getAllUsers() {
         return userService.getAll();
     }
 
-    @GetMapping("/users/{id}")
+    @GetMapping(value="/user/getById/{id}", produces = { "application/json" })
     public UserResponse getUserById(@PathVariable Long id) {
         return userService.getUserById(id);
     }
 
-    @GetMapping("users/{email}")
+    @GetMapping(value="user/getByEmail/{email}", produces = { "application/json" })
     public ResponseEntity<String> getUserByEmail(@PathVariable String email) {
-        return userService.getUserByEmail(email);
+        UserDto user = userService.getUserByEmail(email);
+        if (user != null) {
+            return ResponseEntity.status(HttpStatus.OK).body("User: " + UserMapper.map(user));
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Email doesn't exist");
+        }
     }
 
-    @GetMapping("/user")
+    @GetMapping(value="/user/getByUserName", produces = { "application/json" })
     public ResponseEntity<?> getUserByName(@RequestParam(name = "user", required = false) String username) {
         UserDto userDto = userService.findUserByName(username);
         if (userDto != null) {
@@ -54,17 +56,17 @@ public class UserController {
         }
     }
 
-    @PostMapping("/users")
+    @PostMapping(value="/user/create", consumes={"application/json"}, produces = { "application/json" })
     public ResponseEntity<String> newUser(@RequestBody UserDto user) {
         return userService.addUser(user);
     }
 
-    @DeleteMapping("/users/{id}")
+    @DeleteMapping("/user/delete/{id}")
     public ResponseEntity<String> deleteUserById(@PathVariable("id") Long id) {
         return userService.deleteById(id);
     }
 
-    @PutMapping("/users/{id}")
+    @PutMapping("/user/edit/{id}")
     public ResponseEntity<String> updateUserById(@PathVariable("id") Long id, @RequestBody UserDto user) {
         return userService.updateUserById(id, user);
     }
