35a28a2138a5ff8785e683d3884854f82c2d9e3f
FeliciaSH <felicia.s.hallberg@gmail.com>
2021-11-30 10:09:35 +0100

    removed unused imports

--- src/main/java/com/example/springproject/controller/UserController.java
+++ src/main/java/com/example/springproject/controller/UserController.java
@@ -4,14 +4,10 @@ import com.example.springproject.data.mapper.UserMapper;
 import com.example.springproject.entity.UserDto;
 import com.example.springproject.response.UserResponse;
 import com.example.springproject.service.UserService;
-import jakarta.ws.rs.Produces;
-import org.apache.catalina.User;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
