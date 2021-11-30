d1ae93afa5d1b0a9207532cb7c8e9e0431f35e7d
FeliciaSH <felicia.s.hallberg@gmail.com>
2021-11-30 01:57:10 +0100

    JWT done except for storing secret in a secure way. findByEmail refactored.

--- src/main/java/com/example/springproject/repo/UserRepository.java
+++ src/main/java/com/example/springproject/repo/UserRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<UserDto, Long> {
     UserDto findByUserName(String userName);
+    UserDto findByEmail(String email);
 }
