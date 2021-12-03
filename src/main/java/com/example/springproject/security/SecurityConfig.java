6f90aaa9aec42aeef9e47452ccbdbc14f791e6df
FeliciaSH <felicia.s.hallberg@gmail.com>
2021-12-03 18:56:18 +0100

    added config dependency in pom.xml and custom metadata configuration to specify jwt.key type etc.

--- src/main/java/com/example/springproject/security/SecurityConfig.java
+++ src/main/java/com/example/springproject/security/SecurityConfig.java
@@ -15,7 +15,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
